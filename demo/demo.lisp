0494cebdc021d252677900e2ab9306a0956e0244
Nicola Ferru Aka NFVblog <nicoferru190@gmail.com>
2026-03-09 09:57:54 +0100

    differenza tra il vecchio paradigma e il nuovo

--- demo/demo.lisp
+++ demo/demo.lisp
@@ -6,6 +6,8 @@
 
 (in-package :cl-obelisk)
 
+; vecchio dsl
+
 ;; Definiamo un grafo con un ciclo: A -> B -> A
 (defparameter *appunti-complessi*
   '("Studio"
@@ -21,3 +23,15 @@
 	       :carta '(15 15)
                :orientamento :orizzontale 
                :formato :png)
+
+; nuovo dsl
+(cl-obelisk:genera-da-dsl "mia_mappa" 
+			  '("Obelisk"
+			    (:normal "Creativo"
+			     (:normal "Senza Regole" "Obelisk")) ;; se il nodo esiste fa un collegamento 
+			    (:importante "Semplice" "Veloce"))
+			    :carta :a4
+			    :margine 2
+			    :formato :pdf
+			    :stile :umanistico
+			  )
